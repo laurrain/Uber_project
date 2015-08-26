cdf7fe5cd2b7fd8f7c46e75367d1dc843dc7d57e
Your Name <tchebs.tchebayou1@gmail.com>
2015-08-26 16:34:24 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,99 @@
+'use strict';
+
+var express = require('express'),
+    app = express(),
+    bodyParser = require('body-parser'),
+    http = require('http').Server(app),
+    io = require('socket.io')(http),
+    exphbs  = require('express-handlebars'),
+    mysql = require('mysql'), 
+    myConnection = require('express-myconnection'),  
+    session = require('express-session'),
+    route = require('./routes/route')
+    
+var user = {};
+
+var dbOptions = {
+      host: 'localhost',
+      user: 'root',
+      password: '42926238',
+      port: 3306,
+      database: 'uber_Driver'
+};
+
+app.engine("handlebars", exphbs({defaultLayout:"main"}))
+app.set("view engine", "handlebars")
+
+app.use("/static", express.static("views"))
+app.use(express.static('public'));
+app.use("/static", express.static("."))
+
+//setup middleware
+app.use(myConnection(mysql, dbOptions, 'single'));
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
+app.use(session({secret: "yada yada", saveUninitialized : false, resave: true, cookie : {maxAge : 5*60000}}));
+
+app.get("/", function(req, res){  
+
+  res.render("home",{layout: false});
+})
+
+app.get("/login", function(req, res){
+  res.render("login", {layout : false});
+})
+
+app.post('/login', route.authDriver)
+
+app.get("/logout", function(req, res){
+  delete req.session.user;
+  res.redirect('/login');
+});
+
+app.get("/signup", function(req,res){
+  res.render('signup', {data:route, layout: false})
+})
+app.post('/signup', route.signup)
+
+/*app.get('/driver_handler/:username', function(req, res){
+  res.render('driver', username)
+})*/
+
+app.get('/index', function(req, res){
+  res.render('index', {data: route, layout: false, username : req.session.user})
+  console.log("voila...")
+
+  io.on('connection', function(socket){
+    console.log('a user connected'+ " " +req.session.user);
+    socket.emit('connected');
+      socket.on('disconnect', function(msg){
+        console.log('user disconnected');
+        socket.emit('disconnected');
+    });
+    socket.on('chat message', function(msg){
+    console.log('message: ');
+    io.emit('chat message',req.session.user+ " " +msg);
+    });
+  });
+});
+app.get('/appointment', function(req, res){
+  res.render('appointment', {layout: false})
+})
+
+app.get('/agent', function(req, res){
+  res.render('agent')
+})
+app.get("/*", function(req, res){  
+
+  res.render("home");
+})
+
+var port = process.env.PORT || 5000;
+
+var server = http.listen(port, function(){
+
+  console.log("server is running on " + server.address().address + ":" +server.address().port)
+
+})
---
+++ routes/route.js
@@ -0,0 +1,139 @@
+var bcrypt = require('bcrypt');
+count = 0,
+lock = false;
+exports.authDriver = function(req, res, next){
+	var userData = JSON.parse(JSON.stringify(req.body));
+	var user = userData.username,
+	    password = userData.password;
+
+	req.getConnection(function(err, connection){
+		      if(err) return next(err);
+              //pass
+		connection.query("SELECT * FROM Driver WHERE username = ?", [user], function(err, results){
+        	  if (err){
+                return next(err);
+              } 
+                
+			if (results.length > 0){
+			    
+                var storedUser = results[0];
+                console.log(storedUser);
+
+				// Load password hash from DB
+				bcrypt.compare(password, storedUser.password, function(err, passwordMatched){
+    		// passwordMatched === true
+                    console.log(passwordMatched)
+    				if(passwordMatched == true && results[0].locked == false){
+    					count = 0;
+                        req.session.user = results[0].username
+                        return res.redirect('/index');
+    				}else{
+                        count++;
+                        message = "user or password incorrect";
+                        if(count == 3 || results[0].locked){
+                              connection.query('UPDATE Driver SET locked = ? WHERE username = ?', [true,user], function(err, results) {
+                                if (err) return next(err);
+                            
+                                msg = "Your account has been blocked! Wrong password supplied 3 times!";
+                                return res.render("login", {
+                                    message : msg,
+                                    layout : false
+                                });
+                            });
+                        }else{
+
+                            return res.render("login", {
+                                message : message+"Username or password incorrect!",
+                                layout : false
+                            });
+                        }
+                    }
+                });
+            }
+            else{
+                counter = 0
+                return res.render("login", {
+                    message : "Username doesn't exist!",
+                    layout : false
+                });
+            }
+        });
+    });
+}
+
+/**exports.checkUser = function(req, res, next){
+
+  if (req.session.user){
+   // past_pages.push(req._parsedOriginalUrl.path)
+     res.render("home")
+    
+  }else{
+    // the user is not logged in redirect him to the login page-
+    res.redirect('/login');
+  }
+};**/
+
+
+exports.signup = function (req, res, next) {
+    req.getConnection(function(err, connection){
+        if (err){ 
+            return next(err);
+        }
+        
+        var input = JSON.parse(JSON.stringify(req.body));
+        var data = {
+                    username : input.username,
+                    password : input.password
+            };
+
+			bcrypt.genSalt(10, function(err, salt) {
+    		bcrypt.hash(input.password, salt, function(err, hash) {
+        	// Store hash in your password DB.
+        	if(err)return -1
+        		data.password = hash;
+            if(input.confirm_password == input.password){
+        		connection.query('SELECT * FROM Driver WHERE username = ?', input.username, function(err, results1) {
+                if (results1.length == 0){
+                    connection.query('insert into Driver set ?', data, function(err, results) {
+                            if (err)
+                                    console.log("Error inserting : %s ",err );
+                     
+                            req.session.user = input.username;
+                      
+                            res.render('index');
+                    });
+                }
+                else{
+                    res.render("signup", {
+                                            message : "Username alredy exists!",
+                                            layout : false
+                                            })
+                }
+            });
+        }
+        else{
+            res.render("signup", {
+                message : "Passwords don't match!",
+                layout : false
+            	})
+           	};
+        })	
+        })
+    });
+}
+
+exports.get_driver = function(req, res, next){
+    var id = req.params.id;
+    req.getConnection(function(err, connection){
+        if(err)
+            return next(err);
+
+        connection.query("SELECT username FROM Driver WHERE username = ?", [id], function(err, data){
+            if(err)
+                console.log("[!] Error requesting driver data from database:\n\t%s", err);
+
+
+            res.render("index",{username : data})
+        })
+    })
+}
